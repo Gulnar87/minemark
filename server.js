1f73380aa82b4107520f40be600087be828d280a
gumam <gulnar.mammadova@eonics.nl>
2019-08-02 00:15:21 +0200

    found it

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/gulnarmam'));
+app.use(express.static(__dirname + '/dist/minemark'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/gulnarmam/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/minemark/index.html'));
 });
 
 // default Heroku port
