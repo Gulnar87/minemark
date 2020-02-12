8b216e0a9ebf017c6b2d7df4f34ca1b9e6c7b5b0
Patrick Teulings <patrickteulings@gmail.com>
2020-02-12 14:41:31 +0100

    Added publications and minor updates

--- src/app/network/network.service.ts
+++ src/app/network/network.service.ts
@@ -10,9 +10,9 @@ export class NetworkService {
 
    teamChanged = new Subject<Team[]>();
    advisorsChanged = new Subject<Advisors[]>();
-   
+
 	private	team : Team[] = [
-         {name: 'Lala van der Kolk', profession: 'Co-founder', pic: 'assets/Lala_van_der_Kolk.jpg',  bio: 'Lala is a co-founder of Mine Mark. She worked for the Ministry of Emergency Situations in Azerbaijan where she discovered her interest in Crisis Management. Therefore, she did her Master in Crisis- and Security Management at Leiden University. Now she is working as a Senior Consultant for Deloitte’s Crisis Management and Resilience team.', alt: 'Lala van der Kolk'}, 
+         {name: 'Lala van der Kolk', profession: 'Co-founder', pic: 'assets/Lala_van_der_Kolk.jpg',  bio: 'Lala is a co-founder of Mine Mark. She worked for the Ministry of Emergency Situations in Azerbaijan where she discovered her interest in Crisis Management. Therefore, she did her Master in Crisis- and Security Management at Leiden University. Now she is working as a Senior Consultant for Deloitte’s Crisis Management and Resilience team.', alt: 'Lala van der Kolk'},
          {name: 'Nils Hegel', profession: 'Co-founder', pic: 'assets/Nils_Hegel.jpg', bio: 'Nils is one of Mine Mark’s co-founders and he has a military background. He served in the German Military Police and, in that position, was deployed to several conflict areas like Afghanistan. There he saw first-hand how explosive remnants of war destroy individuals, families and whole communities. Nils is currently studying Security Studies at Leiden University.', alt: 'Nils Hegel' },
          {name: 'Ilaria Vallini', profession: 'Content Creator', pic: 'assets/Ilaria_Vallini.jpg', bio: 'Ilaria is a content creator at Mine Mark. She is passionate about communication and finds visual content a great tool to reach people and raise awareness. Her academic background is in Politics: BA International Relations from Sapienza University of Rome and MSc Middle East Politics from SOAS of London. Ilaria is now working for ASTP, a Knowledge Transfer association of Leiden', alt: 'Ilaria Vallini' },
          {name: 'Aleksandar Zdravkovski', profession: 'Finance and Strategy Manager', pic: 'assets/Aleksandar_Zrdavkovski.jpg', bio: 'Aleksandar is a Finance and Strategy Manager at Mine Mark. He holds an MSc in Financial Economics from Erasmus University Rotterdam and currently works as an FP&A Analyst at Danone. Aleksandar is an avid half-marathon runner and strives for a world in which no children are harmed by explosive remnants of war.', alt: 'Aleksandar Zrdavkovski' },
@@ -20,12 +20,13 @@ export class NetworkService {
 
     private advisors: Advisors[] = [
          {name: 'Christine Ball', profession: 'Cartoonist', pic: 'assets/christine_ball.jpg', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', linkToPage: 'http://ball-design.de/', alt:'Christine Ball' },
-         {name: 'Gulnar Mammadova', profession: 'Front-end web developer', pic: 'assets/gulnar_mammadova.jpg',  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', linkToPage: 'http://gulnarmammadova.com', alt:'Gulnar Mammadova'}, 
+         {name: 'Gulnar Mammadova', profession: 'Front-end web developer', pic: 'assets/gulnar_mammadova.jpg',  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', linkToPage: 'http://gulnarmammadova.com', alt:'Gulnar Mammadova'},
          {name: 'Gulshan Guliyeva', profession: 'Web & graphic designer', pic: 'assets/gulshan_guliyeva.jpg', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', linkToPage: 'https://www.behance.net/gulshanguliyeva/', alt:'Gulshan Guliyeva' },
          {name: 'Ilia Baroutev', profession: 'Advisor', pic: 'assets/Ilia_Baroutev.jpg', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', linkToPage: '', alt:'Ilia Baroutev' },
-         {name: 'Cathy Delhanty', profession: 'Networking expert', pic: 'assets/Cathy_Delhanty.jpg', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', linkToPage: 'https://www.linkedin.com/in/cathydelhanty/', alt:'Cathy Delhanty' },
+         //{name: 'Cathy Delhanty', profession: 'Networking expert', pic: 'assets/Cathy_Delhanty.jpg', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', linkToPage: 'https://www.linkedin.com/in/cathydelhanty/', alt:'Cathy Delhanty' },
+         {name: 'Patrick Teulings', profession: 'Creative developer', pic: 'assets/patrickteulings-new-512.jpg', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', linkToPage: 'https://www.linkedin.com/in/patrickteulings/', alt:'Cathy Delhanty' },
          {name: 'Remi Huigen', profession: 'Digital Strategy', pic: 'assets/remi_huigen.jpg', bio: 'Lnt mollit anim id est laborum', linkToPage: 'https://www.linkedin.com/in/remi-huigen-5621a6a5/?originalSubdomain=nl', alt:'Remi Huigen'},
-       
+
         ]
 
   getTeam(){
@@ -46,9 +47,9 @@ export class NetworkService {
 
 
 
- 
 
 
-  
+
+
 
 
