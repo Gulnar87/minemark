53027ae41eb6247a3a56c841186810982075382c
Patrick Teulings <patrickteulings@gmail.com>
2020-09-08 23:12:35 +0200

    Test: Added https redirect to node-server

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.use (function (req, res, next) {
+  if (req.secure) {
+          // request was via https, so do no special handling
+          next();
+  } else {
+          // request was via http, so redirect to https
+          res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 // Serve static files
 app.use(express.static(__dirname + '/dist/minemark'));
 
@@ -9,6 +19,9 @@ app.use(express.static(__dirname + '/dist/minemark'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/minemark/index.html'));
 });
+
+// app.get('*', function(req, res) {
+//   res.redirect('https://' + req.headers.host + req.url);
 
 // default Heroku port
 app.listen(process.env.PORT || 5000);
