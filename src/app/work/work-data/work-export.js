b58858cfffdb6d3252a53166027b55aebd0c0fb7
Patrick Teulings <patrickteulings@gmail.com>
2021-08-19 11:54:07 +0200

    Added and added content to work-section

--- src/app/work/work-data/work-export.js
+++ src/app/work/work-data/work-export.js
@@ -36,6 +36,14 @@ export const data = [
           "During several lessons with pupils at schools number 1 and 21, detailed information on mine safety was provided, and various educational games were played with the children. MINE MARK's heroes and games made a great impression on the pupils. The positive reaction of the children and the directors on MINE MARK's playful education measures demonstrated that there is an urgent need for updated Mine Risk Education that is also tailored to the specific needs of each target group.",
       },
       {
+         paragraph:
+         "<hr />"
+      },
+      {
+        paragraph:
+        "In April 2021, Mine Mark Team, with the assistance of our partner NGO, <a href='http://en.voices.org.ua/'>Voices of Children</a>, distributed Explosive Ordnance Risk Awareness posters in Ukraine. In particular, the posters were distributed to educational facilities in Myronovsky, Novoluhanka, Svitlodarsk, Hirske, and Zolote cities. The posters were explained to the students and exposed in schools’ hallways and classrooms. Furthermore, the posters were distributed and displayed in two “safe houses”, shelters established by Voices of Children in Svitlodarks and Zaycevo.   "
+      },
+      {
         paragraph:
           "MINE MARK is planning to educate more children in different regions in Ukraine that are contaminated with landmines and other explosive remnants of war.",
       },
@@ -46,15 +54,15 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/minemark-f136c.appspot.com/o/minemark%2Fwork%2FAzerbaijanProject%2FpicTop.jpg?alt=media&token=9a908087-1459-487c-870e-8e42cb690eea",
     pictures: [
       {
-        alt: "Mine Risk Education in Ukraine",
+        alt: "Explosive Ordnance Risk Education in Ukraine",
         carouselPic: "assets/images/work/ukraine/ukraine-2.jpeg",
       },
       {
-        alt: "Mine Risk Education in Ukraine",
+        alt: "Explosive Ordnance Risk Education in Ukraine",
         carouselPic: "assets/images/work/ukraine/ukraine-3.jpeg",
       },
     ],
-    title: "Mine Risk Education in Ukraine",
+    title: "Explosive Ordnance Risk Education in Ukraine",
   },
   {
     alt: "Internally displayed children of Azerbaijan",
@@ -77,8 +85,20 @@ export const data = [
       },
       {
         paragraph:
-          "The project implementation in Azerbaijan demonstrated that there is an urgent need for updated Mine Risk Education that is also tailored to the specific needs of each target group. Mine Mark is planning to educate more children in different regions in Azerbaijan that are contaminated with landmines. ",
+          "The project implementation in Azerbaijan demonstrated that there is an urgent need for updated Mine Risk Education that is also tailored to the specific needs of each target group. Mine Mark is planning to educate more children in different regions in Azerbaijan that are contaminated with landmines.",
       },
+      {
+        paragraph:
+        "<hr />"
+      },
+      {
+        paragraph:
+        "In January 2021, within the framework of cooperation between Mine Mark and the Mine Action Agency of the Republic of Azerbaijan (ANAMA), an awareness-raising project was implemented in Azerbaijan to inform about the dangers associated with explosive remnants of war. Jointly with our local partner, we printed and submitted 3,250 brochures, 1,500 posters and 3,010 stickers."
+      },
+      {
+        paragraph:
+          "ANAMA helped us deliver educational materials to the residents of the districts and villages exposed to the dangers of mines and unexploded ordnances and provided relevant recommendations to local communities on safe behaviour. Currently, the posters are displayed in Goranboy, Goygol, Khojaly refugee settlement, Kalbajar refugee settlement, Tovuz, Gazakh, and Agstafa districts. "
+      }
     ],
     id: 2,
     picCard: "assets/images/work/azerbaijan/idp_school_azerbaijan_2.jpeg",
@@ -95,6 +115,35 @@ export const data = [
           "assets/images/work/azerbaijan/idp_school_azerbaijan_2.jpeg",
       },
     ],
-    title: "Mine Risk Education in Azerbaijan",
+    title: "Explosive Ordnance Risk Education in Azerbaijan",
+  },
+  {
+    alt: "Internally displayed children of Azerbaijan",
+    descriptions: [
+      {
+        paragraph:
+          "In April 2021,  Mine Mark team started implementing a project in our third target country, Bosnia & Herzegovina, which is heavily contaminated by remnants of previous conflicts. The project aimed to support the local schools by distributing explosive ordnance risk education posters to schools and organizing training sessions for teachers. Jointly with our local partner, <a href='http://posavinabezmina.org/'>Posavina bez mina.</a>"
+      },
+      {
+        paragraph:
+          "Mine Mark realized and produced custom-made posters about mine risk education. Together we delivered 87 posters to 9 schools located in Brcko District, in the villages of Zovik, Bukvik, Krepsic, Ulice and Satorovici. In the second part of the project, the training sessions were delivered to 47 school teachers, enabling them to further educate children about the risk of landmines and the recommended behaviour around mines. Part of this project was funded by the generous help of <a href='https://mladibih.nl/'>Mladi BiH </a>, a non-profit organization run by and for Bosnian youths in the Netherlands. ",
+      },
+    ],
+    id: 3,
+    picCard: "assets/images/work/bosnia/project-bih-10.jpg",
+    picTop:
+      "assets/images/work/bosnia/project-bih-10.jpg",
+    pictures: [
+      {
+        alt: "Internally displayed children of Azerbaijan",
+        carouselPic: "assets/images/work/bosnia/project-bih-10.jpg",
+      },
+      {
+        alt: "Internally displayed children of Azerbaijan",
+        carouselPic:
+          "assets/images/work/bosnia/project-bih-14.jpg",
+      },
+    ],
+    title: "Explosive Ordnance Risk Education in Bosnia & Herzegovina",
   },
 ];
